e202519afbd4b22ad3773f423d87b28f9d691ed8
绝版小强 <lewis@toolate.com>
2022-11-16 19:33:29 +0800

    init

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,18 +3,16 @@ import { defineConfig } from 'father';
 export default defineConfig({
   cjs: {
     extraBabelPlugins: ["transform-remove-console"],
-    ignores: ['**/*.bak*'],
     transformer: 'esbuild',
     platform: 'node',
   },
   esm: {
     extraBabelPlugins: ["transform-remove-console"],
-    ignores: ['**/*.bak*'],
     transformer: 'babel'
   },
-  // umd: {
-  //   output: 'dist/unpkg',
-  //   extraBabelPlugins: ["transform-remove-console"],
-  // },
+  umd: {
+    output: 'dist/unpkg',
+    extraBabelPlugins: ["transform-remove-console"],
+  },
   platform: 'browser',
 });
